8c8637c1b61f3494d6dae2f65d91d4378542a7bc
Vincent Emonet <vincent.emonet@gmail.com>
2023-11-06 16:22:29 +0100

    add bin folder

--- install.sh
+++ install.sh
@@ -6,19 +6,25 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install custom theme and enable virtualenv plugin
+# Install custom theme
 curl -fsSL -o ~/.oh-my-zsh/custom/themes/biratime.zsh-theme https://raw.github.com/vemonet/biratime/main/biratime.zsh-theme
 sed -i 's/^ZSH_THEME=".*"$/ZSH_THEME="biratime"/g' ~/.zshrc
+
+
+# Enable virtualenv plugin
 sed -i 's/^plugins=(/plugins=(virtualenv /g' ~/.zshrc
 
+
 # Install Solarized colors
-git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized
-./install.sh
+# git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
+# cd gnome-terminal-colors-solarized
+# ./install.sh
+
 
 # Hide the default conda env displayed to only show it with the ZSH theme
 echo "\`conda config --set changeps1 false\`" >> ~/.oh-my-zsh/plugins/virtualenv/virtualenv.plugin.zsh
 
-# Enable the ZSH shell by default
-sudo chsh --shell=/usr/bin/zsh $USER
+echo 'setopt NO_HUP' >> ~/.zshrc
 
+# Enable the ZSH shell by default
+chsh --shell=/usr/bin/zsh $USER
